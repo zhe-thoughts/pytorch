3e96e581f8aecffa541ef43b25900ab216665685
Joel Schlosser <jbschlosser@meta.com>
2025-02-10 12:21:32 -0500

    Use inductor backend for NJT compile tests
    ghstack-source-id: e6925fa187bcf64f1fbf9922be6239fca8b91021
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/146721

--- test/test_nestedtensor.py
+++ test/test_nestedtensor.py
@@ -8631,6 +8631,7 @@ class TestNestedTensorOpInfo(NestedTensorTestCase):
 
                     self.assertEqualNoncontigAware(grads, grads_ref)
 
+    @skipCUDAIf(not SM70OrLater, "GPU capability is < SM70")
     @ops(
         [op for op in njt_op_db if op.supports_njt],
         allowed_dtypes=(torch.float32,),
@@ -8651,9 +8652,7 @@ class TestNestedTensorOpInfo(NestedTensorTestCase):
                 def f(*args, **kwargs):
                     return op_fn(*args, **kwargs)
 
-                compiled_f = torch.compile(
-                    f, fullgraph=True, backend="aot_eager_decomp_partition"
-                )
+                compiled_f = torch.compile(f, fullgraph=True, backend="inductor")
 
                 out_ref = f(sample.input, *sample.args, **sample.kwargs)
                 out_compile = compiled_f(sample.input, *sample.args, **sample.kwargs)
@@ -8676,7 +8675,7 @@ class TestNestedTensorOpInfo(NestedTensorTestCase):
                         return op_fn(*args, **kwargs)
 
                     compiled_in_f = torch.compile(
-                        in_f, fullgraph=True, backend="aot_eager_decomp_partition"
+                        in_f, fullgraph=True, backend="inductor"
                     )
 
                     compiled_in_f(sample.input, *sample.args, **sample.kwargs)
@@ -8685,6 +8684,7 @@ class TestNestedTensorOpInfo(NestedTensorTestCase):
                     else:
                         self.assertEqual(sample.input, out_ref)
 
+    @skipCUDAIf(not SM70OrLater, "GPU capability is < SM70")
     @ops(
         [op for op in njt_op_db if op.supports_njt and op.supports_autograd],
         allowed_dtypes=(torch.float32,),
@@ -8705,9 +8705,7 @@ class TestNestedTensorOpInfo(NestedTensorTestCase):
                 def f(*args, **kwargs):
                     return op_fn(*args, **kwargs)
 
-                compiled_f = torch.compile(
-                    f, fullgraph=True, backend="aot_eager_decomp_partition"
-                )
+                compiled_f = torch.compile(f, fullgraph=True, backend="inductor")
 
                 out_ref = f(sample.input, *sample.args, **sample.kwargs)
                 out_compile = compiled_f(sample.input, *sample.args, **sample.kwargs)
