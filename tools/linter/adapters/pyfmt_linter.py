1e3d0d6246936cfa7c41ab72f130188f30e31ebc
Xuehai Pan <XuehaiPan@pku.edu.cn>
2025-03-02 04:57:24 +0800

    [BE][PYFMT] migrate PYFMT for `torch/[a-c]*/` to `ruff format`
    ghstack-source-id: 0923ba3e3bdc617b3c34cdabe24a4bfcbc8f3226
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/144554

--- tools/linter/adapters/pyfmt_linter.py
+++ tools/linter/adapters/pyfmt_linter.py
@@ -56,9 +56,6 @@ USE_BLACK_FILELIST = re.compile(
                     # torch/_[j-z]*/**
                     "torch/_[j-z]*/**",
                     # torch/[a-c]*/**
-                    "torch/a[a-n]*/**",
-                    "torch/a[p-z]*/**",
-                    "torch/[b-c]*/**",
                     # torch/d*/**
                     # torch/[e-n]*/**
                     "torch/[e-n]*/**",
