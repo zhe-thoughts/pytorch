09e7c5e9056baaec30b76e503b479b97504e59d5
Sam Larsen <slarsen@meta.com>
2025-03-05 09:47:19 -0800

    Re-enable test_torchinductor:test_buffer_batch_norm
    Summary: Per https://github.com/pytorch/pytorch/issues/128198 seems like this is working now
    Fixes #128198
    
    ghstack-source-id: 79ffaf80f060a87c6b9afc3945e5e437461ded5a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/148573

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -92,7 +92,6 @@ from torch.testing._internal.common_utils import (
     MACOS_VERSION,
     parametrize,
     serialTest,
-    skipIfNNModuleInlined,
     skipIfRocm,
     skipIfWindows,
     skipIfXpu,
@@ -4888,7 +4887,6 @@ class CommonTemplate:
 
         self.assertEqual(eager_delta, compile_delta)
 
-    @skipIfNNModuleInlined("https://github.com/pytorch/pytorch/issues/128198")
     def test_buffer_batch_norm(self):
         class MyModel(torch.nn.Module):
             def __init__(self) -> None:
