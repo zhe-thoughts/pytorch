7e348c7586366bbb4480f31c84d89ce9eff476f1
George White <georgew@graphcore.ai>
2025-03-10 08:08:42 +0000

    Enable content store for XPU but not MPS

--- torch/utils/_content_store.py
+++ torch/utils/_content_store.py
@@ -97,10 +97,8 @@ def hash_storage(storage: torch.UntypedStorage, *, stable_hash: bool = False) ->
     from torch._dynamo.utils import is_compile_supported
 
     device_type = storage.device.type
-    # FIXME: XPU supports Inductor (and Triton), but is not handled below, so
-    # use the same fallback as other device types not supported by Inductor for
-    # now.
-    if stable_hash or not is_compile_supported(device_type) or device_type == "xpu":
+    # FIXME: MPS does not yet support some of the ops required for hashing
+    if stable_hash or not is_compile_supported(device_type) or device_type == "mps":
         cpu_storage = storage.cpu()
         # TODO: make storage support buffer protocol so this isn't
         # necessary
