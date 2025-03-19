3246924fbcdeef740cec228bef9442e163f54d4a
Yidi Wu <yidi@meta.com>
2025-03-19 15:01:34 -0700

    Avoid graph break on torch.__future__.get_swap_module_params_on_conversion
    ghstack-source-id: af86ff1a6ab0401734d71d6e00edee16d355c7ff
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/149561

--- torch/_dynamo/variables/builder.py
+++ torch/_dynamo/variables/builder.py
@@ -2650,6 +2650,7 @@ def handle_traced_output(example_value, tx, proxy, options, subclass_type, targe
             torch.backends.cuda.is_flash_attention_available,
             torch.backends.cuda.can_use_flash_attention,
             torch.backends.cuda.can_use_efficient_attention,
+            torch.__future__.get_swap_module_params_on_conversion,
             "is_integer",
         ]
         + list(supported_const_comparison_op_values.keys())
